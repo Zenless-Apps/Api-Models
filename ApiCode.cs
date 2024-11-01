30e0f6df3203ad5ae34179e6ce22c07ca40c1d8c
JuanCalle1606 <jpmilojp@gmail.com>
2024-11-01 00:01:54 -0500

    Add doc

--- ApiCode.cs
+++ ApiCode.cs
@@ -1,5 +1,10 @@
 namespace ZZZ.ApiModels;
 
+/// <summary>
+/// There are some codes that are returned by the API.
+///
+/// Is possible that the API returns a code that is not listed here, these are the most common codes.
+/// </summary>
 public enum ApiCode {
 	Ok = 0,
 	Error = 1,
