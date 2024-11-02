7c8cdc43c8d5f4ae2bbea04d8618b1686ca0e13a
JuanCalle1606 <jpmilojp@gmail.com>
2024-11-01 23:35:54 -0500

    Added Engines Enum

---
+++ Engines.cs
@@ -0,0 +1,83 @@
+namespace ZZZ.ApiModels
+{
+	/// <summary>
+	/// List of engines that can be used in the game.
+	/// </summary>
+	public enum Engines
+	{
+		#region Support
+		Mark1 = -1,
+		Mark2 = 12005,
+		Mark3 = -2,
+        
+		GameBall = -3,
+		TheVault = 13103,
+		SliceOfTime = -4,
+		Kaboom = -5,
+		BashfulDemon = 13113,
+        
+		Cradle = 14121,
+		#endregion
+        
+		#region Attack
+		Pleniluna = -6,
+		Noviluna = -7,
+		Decrescent = -8,
+        
+		Superstar = -9,
+		StarlightReplica = -10,
+		Starlight = 13004,
+		Housekeeper = 13106,
+		Blossom = 13013,
+		Drill = 13111,
+		Cannon = -11,
+        
+		Brimstone = -12,
+		Cushion = -13,
+		Suppressor = -14,
+		DeepSea = 14119,
+		#endregion
+        
+		#region Defense
+		Inflection = -15,
+		Base = -16,
+        
+		Spring = -17,
+		Peacekeeper = 13127,
+		Transmorpher = -18,
+		Bunny = -19,
+		Cylinder = -20,
+        
+		TuskOfFury = -21,
+		#endregion
+        
+		#region Anomaly
+		Charlie = -22,
+		Bravo = -23,
+		Alpha = -24,
+        
+		Gemini = 13008,
+		Roaring = -25,
+		Rainforest = -26,
+		ElectroLip = -27,
+        
+		Stinger = -28,
+		Compiler = 14118,
+		#endregion
+        
+		#region Stun
+		Revolver = -29,
+		Hatchet = -30,
+		Arrow = -31,
+        
+		Steam = -32,
+		Shooter = 14003,
+		Rock = -33,
+		Battery = -34,
+        
+		Restrained = -35,
+		Teapot = 14125,
+		Hellfire = -36
+		#endregion
+	}
+}
--- Responses/Agent.cs
+++ Responses/Agent.cs
@@ -1,74 +1,73 @@
+// ReSharper disable UnusedAutoPropertyAccessor.Global
 namespace ZZZ.ApiModels.Responses;
-
-using System.Text.Json.Nodes;
 
 public class Agent {
 
-	public int Id { get; set; }
+	public int Id { get; init; }
 
-	public int Level { get; set; }
+	public int Level { get; init; }
 
-	public int Cinema { get; set; }
+	public int Cinema { get; init; }
 
-	public Weapon? Engine { get; set; }
+	public Weapon? Engine { get; init; }
 
-	public required string Name { get; set; }
+	public required string Name { get; init; }
 
 	public class Weapon {
 		
-		public int Id { get; set; }
+		public Engines Id { get; init; }
 		
-		public int Level { get; set; }
+		public int Level { get; init; }
 
-		public int Refinement { get; set; }
+		public int Refinement { get; init; }
 
-		public required string Name { get; set; }
+		public required string Name { get; init; }
 	}
 
-	public required Mindscape[] Cinemas { get; set; }
+	public required Mindscape[] Cinemas { get; init; }
 	
 	public class Mindscape {
 		
-		public int Id { get; set; }
+		public int Id { get; init; }
 		
-		public int Pos { get; set; }
+		public int Pos { get; init; }
 
-		public bool Unlocked { get; set; }
+		public bool Unlocked { get; init; }
 	}
 	
-	public required Skill[] Skills { get; set; }
+	public required Skill[] Skills { get; init; }
 	
 	public class Skill {
 		
-		public Skills Id { get; set; }
+		public Skills Id { get; init; }
 		
-		public int Level { get; set; }
+		public int Level { get; init; }
 
-		public required string Title { get; set; }
+		public required string Title { get; init; }
 	}
 	
-	public required Disc[] Discs { get; set; }
+	public required Disc[] Discs { get; init; }
 	
 	public class Disc {
 		
-		public Discs Id { get; set; }
+		public Discs Id { get; init; }
 		
-		public int Level { get; set; }
+		public int Level { get; init; }
 		
-		public required string Name { get; set; }
+		public required string Name { get; init; }
 		
-		public required ItemRank Rank { get; set; }
+		public required ItemRank Rank { get; init; }
 		
-		public int Slot { get; set; }
+		public int Slot { get; init; }
 		
-		public required Stat MainStat { get; set; }
+		public required Stat MainStat { get; init; }
 		
-		public required Stat[] SubStats { get; set; }
+		public required Stat[] SubStats { get; init; }
 		
 		public class Stat {
-			public DiscStats Id { get; set; }
-			public required string Name { get; set; }
-			public required string Value { get; set; }
+			public DiscStats Id { get; init; }
+			public required string Name { get; init; }
+			public required string Value { get; init; }
 		}
 	}
 }
