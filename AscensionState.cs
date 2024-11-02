c6e455d34bc7bce963a89ccdb97d50bd2439b191
JuanCalle1606 <jpmilojp@gmail.com>
2024-11-02 18:19:51 -0500

    Add ascension enum

---
+++ AscensionState.cs
@@ -0,0 +1,31 @@
+// ReSharper disable InconsistentNaming
+namespace ZZZ.ApiModels;
+
+using System.ComponentModel.DataAnnotations;
+
+public enum AscensionState {
+	[Display(Description = "0/10")]
+	A0_10,
+	[Display(Description = "10/10")]
+	A10_10,
+	[Display(Description = "10/20")]
+	A10_20,
+	[Display(Description = "20/20")]
+	A20_20,
+	[Display(Description = "20/30")]
+	A20_30,
+	[Display(Description = "30/30")]
+	A30_30,
+	[Display(Description = "30/40")]
+	A30_40,
+	[Display(Description = "40/40")]
+	A40_40,
+	[Display(Description = "40/50")]
+	A40_50,
+	[Display(Description = "50/50")]
+	A50_50,
+	[Display(Description = "50/60")]
+	A50_60,
+	[Display(Description = "60/60")]
+	A60_60,
+}
--- Responses/Agent.cs
+++ Responses/Agent.cs
@@ -1,11 +1,39 @@
 // ReSharper disable UnusedAutoPropertyAccessor.Global
 namespace ZZZ.ApiModels.Responses;
 
+using System.Text.Json.Serialization;
+
 public class Agent {
+	readonly int _level;
 
 	public Agents Id { get; init; }
 
-	public int Level { get; init; }
+	[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+	public int Level
+	{
+		get => _level;
+		init
+		{
+			_level = value;
+			Ascension = _level switch
+			{
+				>= 60 => AscensionState.A60_60,
+				> 50 => AscensionState.A50_60,
+				50 => AscensionState.A50_50,
+				> 40 => AscensionState.A40_50,
+				40 => AscensionState.A40_40,
+				> 30 => AscensionState.A30_40,
+				30 => AscensionState.A30_30,
+				> 20 => AscensionState.A20_30,
+				20 => AscensionState.A20_20,
+				> 10 => AscensionState.A10_20,
+				10 => AscensionState.A10_10,
+				_ => AscensionState.A0_10
+			};
+		}
+	}
+
+	public AscensionState Ascension { get; init; }
 
 	public int Cinema { get; init; }
 
@@ -14,9 +42,9 @@ public class Agent {
 	public required string Name { get; init; }
 
 	public class Weapon {
-		
+
 		public Engines Id { get; init; }
-		
+
 		public int Level { get; init; }
 
 		public int Refinement { get; init; }
@@ -24,49 +52,53 @@ public class Agent {
 		public required string Name { get; init; }
 	}
 
-	public required Mindscape[] Cinemas { get; init; }
-	
+	[Obsolete("Use Cinema instead")]
+	public Mindscape[] Cinemas { get; init; } = [];
+
+	[Obsolete("This class is redundant, use Cinema instead")]
 	public class Mindscape {
-		
+
 		public int Id { get; init; }
-		
+
 		public int Pos { get; init; }
 
 		public bool Unlocked { get; init; }
 	}
-	
+
 	public required Skill[] Skills { get; init; }
-	
+
 	public class Skill {
-		
+
 		public Skills Id { get; init; }
-		
+
 		public int Level { get; init; }
 
 		public required string Title { get; init; }
 	}
-	
+
 	public required Disc[] Discs { get; init; }
-	
+
 	public class Disc {
-		
+
 		public Discs Id { get; init; }
-		
+
 		public int Level { get; init; }
-		
+
 		public required string Name { get; init; }
-		
+
 		public required ItemRank Rank { get; init; }
-		
+
 		public int Slot { get; init; }
-		
+
 		public required Stat? MainStat { get; init; }
-		
+
 		public required Stat[] SubStats { get; init; }
-		
+
 		public class Stat {
 			public DiscStats Id { get; init; }
+
 			public required string Name { get; init; }
+
 			public required string Value { get; init; }
 		}
 	}
