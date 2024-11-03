8a5e81dbdc81a753fd278281af3023da0affbd47
JuanCalle1606 <jpmilojp@gmail.com>
2024-11-02 19:31:47 -0500

    Added ascension to engine and rolls to discs

--- Responses/Agent.cs
+++ Responses/Agent.cs
@@ -15,22 +15,26 @@ public class Agent {
 		init
 		{
 			_level = value;
-			Ascension = _level switch
-			{
-				>= 60 => AscensionState.A60_60,
-				> 50 => AscensionState.A50_60,
-				50 => AscensionState.A50_50,
-				> 40 => AscensionState.A40_50,
-				40 => AscensionState.A40_40,
-				> 30 => AscensionState.A30_40,
-				30 => AscensionState.A30_30,
-				> 20 => AscensionState.A20_30,
-				20 => AscensionState.A20_20,
-				> 10 => AscensionState.A10_20,
-				10 => AscensionState.A10_10,
-				_ => AscensionState.A0_10
-			};
+			Ascension = A(_level);
 		}
+	}
+
+	static AscensionState A(int level) {
+		return level switch
+		{
+			>= 60 => AscensionState.A60_60,
+			> 50 => AscensionState.A50_60,
+			50 => AscensionState.A50_50,
+			> 40 => AscensionState.A40_50,
+			40 => AscensionState.A40_40,
+			> 30 => AscensionState.A30_40,
+			30 => AscensionState.A30_30,
+			> 20 => AscensionState.A20_30,
+			20 => AscensionState.A20_20,
+			> 10 => AscensionState.A10_20,
+			10 => AscensionState.A10_10,
+			_ => AscensionState.A0_10
+		};
 	}
 
 	public AscensionState Ascension { get; init; }
@@ -42,14 +46,27 @@ public class Agent {
 	public required string Name { get; init; }
 
 	public class Weapon {
-
+		readonly int _level;
+		
 		public Engines Id { get; init; }
 
-		public int Level { get; init; }
+		[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+		public int Level
+		{
+			get => _level;
+			init
+			{
+				_level = value;
+				Ascension = A(_level);
+			}
+		}
+
 
 		public int Refinement { get; init; }
 
 		public required string Name { get; init; }
+
+		public AscensionState Ascension { get; set; }
 	}
 
 	[Obsolete("Use Cinema instead")]
@@ -100,6 +117,8 @@ public class Agent {
 			public required string Name { get; init; }
 
 			public required string Value { get; init; }
+
+			public int Rolls { get; init; } = -1;
 		}
 	}
 }
