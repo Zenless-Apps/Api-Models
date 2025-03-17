a0398459a77688a19612b2f7b1bff28cd84fef5a
JuanCalle1606 <jpmilojp@gmail.com>
2025-03-16 19:19:34 -0500

    Added new skills types

--- Skills.cs
+++ Skills.cs
@@ -10,11 +10,14 @@ public enum Skills
 	 * Alternative version of skills.
 	 * Not returned by the api but used in the calculator client.
 	 */
-	Ex = -5,
+	Ex = -50,
 	Dash,
 	Ultimate,
 	Additional,
 	Quick,
+	
+	Anomaly = -10,
+	Disorder,
 	
 	/*
 	 * Skills returned by the api.
